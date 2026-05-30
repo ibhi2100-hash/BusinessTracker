39df60a7d78cd4753ee9be2994a1683b4e91cddd
ibhi2100@gmail.com <ibhi2100@gmail.com>
2026-05-30 05:32:22 +0100

    fixing the inventory update

--- shop-frontend/app/page.tsx
+++ shop-frontend/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
   useEffect(() => {
@@ -11,8 +12,19 @@ const Page = () => {
         .catch((err) => console.error("SW Error", err));
     }
   }, []);
+  const router = useRouter();
+  const handleRegister = () => {
+    router.push("/register")
+  }
 
-  return <div>page</div>;
+  return(
+  <div>
+    <h1  className="text-3xl font-bold mb-4">Welcome to Business Tracker</h1>
+    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={handleRegister}>Get Started</button>
+  </div>
+
+  )
+
 };
 
 export default Page;
