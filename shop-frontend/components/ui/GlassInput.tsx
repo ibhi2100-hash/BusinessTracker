97e46b1f4b65a740f78d78c0ed98f862f3dd7abb
ibhi2100-hash <ibhi2100@gmail.com>
2026-09-15 11:47:40 +0100

    Update GlassInput.tsx

--- shop-frontend/components/ui/GlassInput.tsx
+++ shop-frontend/components/ui/GlassInput.tsx
@@ -1,64 +1,68 @@
+"use client";
+import {
+  forwardRef,
+  type InputHTMLAttributes,
+  type ReactNode,
+} from "react";
 import { cn } from "@/lib/utils";
-
 interface Props
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  icon?: React.ReactNode;
+  extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: ReactNode;
 }
-
-export function GlassInput({
-  icon,
-  className,
-  ...props
-}: Props) {
+export const GlassInput = forwardRef<
+  HTMLInputElement,
+  Props
+>(function GlassInput(
+  {
+    icon,
+    className,
+    ...props
+  },
+  ref
+) {
   return (
     <div className="relative">
       {icon && (
         <div
           className="
-          absolute
-          left-3
-          top-1/2
-          -translate-y-1/2
-          text-gray-400
+            pointer-events-none
+            absolute
+            left-3
+            top-1/2
+            z-10
+            -translate-y-1/2
+            text-gray-400
           "
         >
           {icon}
         </div>
       )}
-
       <input
+        ref={ref}
         className={cn(
           `
-          w-full
-
-          rounded-2xl
-
-          border
-          border-white/10
-
-          bg-white/[0.04]
-
-          backdrop-blur-xl
-
-          py-3
-
-          outline-none
-
-          text-white
-
-          placeholder:text-gray-500
-
-          focus:border-teal-500
-          focus:ring-2
-          focus:ring-teal-500/20
+            w-full
+            rounded-2xl
+            border
+            border-white/10
+            bg-white/[0.04]
+            py-3
+            text-white
+            outline-none
+            backdrop-blur-xl
+            placeholder:text-gray-500
+            focus:border-teal-500
+            focus:ring-2
+            focus:ring-teal-500/20
           `,
-
-          icon ? "pl-10" : "px-4",
-
+          icon
+            ? "pl-10"
+            : "px-4",
           className
         )}
         {...props}
       />
     </div>
   );
-}
+});
+GlassInput.displayName = "GlassInput";
